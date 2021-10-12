f29b0f834c9161d0ad993e2e7285f09cf86961f2
wh75er <wh75er@gmail.com>
2021-10-12 13:24:44 +0300

    change config

--- frontend/src/consts/config.js
+++ frontend/src/consts/config.js
@@ -1,2 +1,2 @@
-// export const gatewayLocation = "http://gateway.bookinghotelservice.ru:10000"
-export const gatewayLocation = "http://0.0.0.0:10000"
+export const gatewayLocation = "http://gateway.bookinghotelservice.ru:10000"
+// export const gatewayLocation = "http://0.0.0.0:10000"
